12c6a5da1c7d7b3fd8a68f361e2b775b4acba865
doyoung park <asdlkj0104@gmail.com>
2024-08-22 16:51:09 +0900

    Update 240822_3

--- models/productModel.js
+++ models/productModel.js
@@ -12,27 +12,4 @@ const productSchema = new mongoose.Schema({
 
 const Product = mongoose.model('Product', productSchema);
 
-class ProductModel {
-    static async getAllProducts() {
-        return await Product.find(); // 모든 제품을 가져옵니다.
-    }
-
-    static async findById(id) {
-        return await Product.findById(id); // ID로 제품을 검색합니다.
-    }
-
-    static async createProduct(productData) {
-        const product = new Product(productData);
-        return await product.save(); // 새로운 제품을 생성하고 저장합니다.
-    }
-
-    static async updateProduct(id, updatedData) {
-        return await Product.findByIdAndUpdate(id, updatedData, { new: true }); // 제품 정보를 업데이트합니다.
-    }
-
-    static async deleteProduct(id) {
-        return await Product.findByIdAndDelete(id); // 제품을 삭제합니다.
-    }
-}
-
-export default ProductModel;
+export default Product;
