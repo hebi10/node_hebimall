980a7b6d82b808887628090bd523b3ba16083109
doyoung park <asdlkj0104@gmail.com>
2024-10-10 17:50:18 +0900

    Update 241010

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,7 +10,11 @@ export const login = async (req, res) => {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({
+            userId: user.userId,
+            nickname: user.nickname,
+            role: user.role,
+          }, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.json({ token });
     } catch (err) {
         res.status(500).json({ message: err.message });
