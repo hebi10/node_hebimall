d920029812d8fe950dcdeb26f837e36afade4c7b
doyoung park <asdlkj0104@gmail.com>
2024-10-10 17:46:24 +0900

    Update 241010

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,14 +3,14 @@ import User from '../models/userModel.js';
 
 export const login = async (req, res) => {
     const { username, password } = req.body;
-
+    
     try {
         const user = await User.findOne({ username });
         if (!user || user.password !== password) {
             return res.status(401).json({ message: 'Invalid credentials' });
         }
 
-        const token = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: '1h' });
         res.json({ token });
     } catch (err) {
         res.status(500).json({ message: err.message });
