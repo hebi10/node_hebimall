f4c20461e8bb241fd50ba8474211c9178c1f1b54
doyoung park <asdlkj0104@gmail.com>
2024-10-11 10:51:04 +0900

    Update 241011

--- models/userModel.js
+++ models/userModel.js
@@ -4,6 +4,9 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    nickname: { type: String, required: true },
+    userId: { type: String, required: true, unique: true },
+    role: { type: String, required: true, enum: ['user', 'admin'], default: 'user' }, 
     createdAt: { type: Date, default: Date.now }
 });
 
