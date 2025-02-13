d3aa28baf953141334c3a9862fa988ebf4db6669
doyoung park <asdlkj0104@gmail.com>
2025-02-13 18:24:04 +0900

    Update

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ import postsRoutes from './routes/posts.js';
 // 필요한 다른 라우트들도 동일하게 가져오기
 
 const app = express();
+const cookieParser = require('cookie-parser');
 
 // CORS 설정
 const allowedOrigins = [
