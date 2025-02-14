64032a5861b5bd41ed484a24d12ae672e3dcefb3
doyoung park <asdlkj0104@gmail.com>
2025-02-14 10:54:02 +0900

    Update

--- routes/cart.js
+++ routes/cart.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getCart, addToCart, updateCartItem, deleteCartItem } from '../controllers/cartController.js';
+import { getCart, addToCart, updateCartItem, removeFromCart } from '../controllers/cartController.js';
 
 const router = express.Router();
 
 router.get('/', getCart);
 router.post('/', addToCart);
 router.put('/:id', updateCartItem);
-router.delete('/:id', deleteCartItem);
+router.delete('/:id', removeFromCart);
 
 export default router;
